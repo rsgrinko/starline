9fb6b2a1907a59c35fceaeb8b90f6671903d0b15
rsgrinko <rsgrinko@gmail.com>
2025-02-06 17:50:16 +0300

    [*] обновление пакетов composer [*] корректировки класса Starline [*] мелкие правки примера [*] codeStyle

--- src/Starline/Examples/test.php
+++ src/Starline/Examples/test.php
@@ -1,45 +1,77 @@
 <?php
+    require_once __DIR__ . '/../../../vendor/autoload.php';
 
-require_once __DIR__ . '../../../vendor/autoload.php';
+    // Настройки доступа из https://my.starline.ru/developer.
+    $config = (new \Starline\Config())
+        ->setLogin('login')           //логин пользователя.
+        ->setPassword('password')  //пароль пользователя.
+        ->setAppId('app id')         //идентификатор приложения.
+        ->setSecret('secret key');   //ключ доступа к приложению.
 
-//Настройки доступа из https://my.starline.ru/developer.
-$config = (new \Starline\Config())
-    ->setLogin('login')//логин пользователя.
-    ->setPassword('password')//пароль пользователя.
-    ->setAppId('app id')//идентификатор приложения.
-    ->setSecret('secret key');//ключ доступа к приложению.
+    $starLine = new \Starline\Starline();
+    $starLine->setConfig($config);
 
-$starline = new \Starline\Starline();
-$starline->setConfig($config);
+    // Пример получения кода приложения.
 
-//Пример получения кода приложения.
-$code = $starline->fetchCode();
+    try {
+        $code = $starLine->fetchCode();
+    } catch (Throwable $e) {
+        die('Не удалось получить код приложения');
+    }
 
-//Пример получения token ключа.
-$token = $starline->fetchToken($code);
+    // Пример получения token ключа.
+    try {
+        $token = $starLine->fetchToken($code);
+    } catch (Throwable $e) {
+        die('Не удалось получить токен ключа');
+    }
 
-//Авторизация пользователя.
-$user_token = $starline->fetchUserToken($token);
+    // Авторизация пользователя.
+    try {
+        $userToken = $starLine->fetchUserToken($token);
+    } catch (Throwable $e) {
+        die('Не удалось произвести авторизацию');
+    }
 
-//Пример получения SLNET token ключа.
-[$slnet, $user_id] = $starline->fetchSLNETToken($user_token);
+    // Пример получения SLNET token ключа.
 
-//Пример получения существующих устройств пользователя.
-$devices = $starline->fetchDevicesInfo($slnet, $user_token, $user_id);
-echo '<pre>';
-print_r($devices);
-echo '</pre>';
+    try {
+        $slnetData = $starLine->fetchSLNETToken($userToken);
+    } catch (Throwable $e) {
+        die('Не удалось получить slnet токен');
+    }
 
-//Пример получения device_id, выберите нужное устройство из массива $devices['user_data']['devices']
-$device_id = $devices['user_data']['devices'][0]['device_id'] ?? '';
+    if (empty($slnetData)) {
+        die('Не удалось получить slnet токен');
+    }
+    [$slnet, $userId] = $slnetData;
 
-//Пример выполнения запроса к устройству.
-$response = $starline->runQuery($slnet, $device_id, [
-    'type' => 'arm',//тип "охраны устройства"
-    'arm' => 1,//постановка на охрану
-]);
-echo '<pre>';
-print_r($response);
-echo '</pre>';
+    // Пример получения существующих устройств пользователя.
+    $devices = [];
+    try {
+        $devices = $starLine->fetchDevicesInfo($slnet, $userToken, $userId);
+    } catch (Throwable $e) {
+        echo 'Не удалось получить slnet токен' . PHP_EOL;
+    }
+    echo '<pre>' . print_r($devices, true) . '</pre>';
+
+    // Пример получения device_id, выберите нужное устройство из массива $devices['user_data']['devices']
+    $deviceId = $devices['user_data']['devices'][0]['device_id'] ?? '';
+
+    // Пример выполнения запроса к устройству.
+    $response = [];
+    try {
+        $response = $starLine->runQuery(
+            $slnet,
+            $deviceId,
+            [
+                'type' => 'arm',// Тип команды: охрана устройства
+                'arm'  => 1,    // Свойство: Включено
+            ]
+        );
+    } catch (Throwable $e) {
+        echo 'Не удалось выполнить команду' . PHP_EOL;
+    }
+    echo '<pre>'. print_r($response, true) . '</pre>';
 
 
